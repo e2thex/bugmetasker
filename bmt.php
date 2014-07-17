1c0fb8dc1ccdec2ac949b940df021c83706995a9
e2thex <erik@e2thex.org>
2014-07-17 13:38:07 -0500

    updated with a fixed file

--- bmt.php
+++ bmt.php
@@ -4,7 +4,8 @@ require_once $dir . "/BugMeTasker.php";
 require_once $dir . "/BugMeTaskerMessager.php";
 
 $action = isset($argv[1]) ? $argv[1] : NULL;
-$t = new BugMeTasker();
+$file = getenv("HOME") . "/.bmttasks";
+$t = new BugMeTasker($file);
 $t->load();
 $t->main($action, new GMessageMessager());
 $t->save();
